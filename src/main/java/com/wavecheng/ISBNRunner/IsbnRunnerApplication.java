f6fe3c70c5f39a92b9ea40d160b9f7a24bda35e8
bo.chen <wavecheng@gmail.com>
2018-03-22 22:22:43 +0800

    init maven project

---
+++ src/main/java/com/wavecheng/ISBNRunner/IsbnRunnerApplication.java
@@ -0,0 +1,12 @@
+package com.wavecheng.ISBNRunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsbnRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsbnRunnerApplication.class, args);
+	}
+}
